528582dd740e5927d992b093acd9a2c5d9bca43a
Ciarán Coughlan <ciaran@coughlanweb.ie>
2022-05-09 08:58:59 +0100

    Files Added

---
+++ data.js
@@ -0,0 +1,107 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gs__0286",
+      "name": "GS__0286",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.4832735861433157,
+        "pitch": -0.1600587811557599,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.623731782879343,
+          "pitch": -0.12299545792679467,
+          "rotation": 0,
+          "target": "1-gs__0288"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4832735861433157,
+          "pitch": -0.1600587811557599,
+          "title": "Boiler",
+          "text": "Pre-heated water from the heat exchanger is stored and heated here"
+        }
+      ]
+    },
+    {
+      "id": "1-gs__0288",
+      "name": "GS__0288",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.2406893577028164,
+        "pitch": -0.29859767548667193,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3715096134762135,
+          "pitch": -0.17192425500135755,
+          "rotation": 0,
+          "target": "0-gs__0286"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.054045525839798714,
+          "pitch": -0.23904466786076028,
+          "title": "Kitchen Drain &amp; Heat Exchanger",
+          "text": "The kitchen drain was been replaced with a copper heat exchanger which pre-heats cold water. A pump moves the cold water to the boiler for storage and further heating from the heat exchanger.&nbsp;&nbsp;"
+        },
+        {
+          "yaw": -2.600034269848271,
+          "pitch": -0.41626650134880094,
+          "title": "Monitoring System",
+          "text": "A remote monitoring system that collects data about the performance of the system for future research and improvements.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
